b45d5e4154796ca5a304d86edee5afa739573e9e
Nirali Dadhaniya <niralidadhaniya2001@gmail.com>
2022-09-30 18:26:19 +0530

    30/9/2022

---
+++ Training/resources/views/includes/sidebar.blade.php
@@ -0,0 +1,9 @@
+ <div class="col-2">
+  <!-- The sidebar -->
+      <div class="sidebar ">
+        <a class="active" href="/">Home</a>
+        <a href="/aboutus">About Us</a>
+        <a href="/service">Service</a>
+        <a href="/contact">Contact</a></a>
+      </div>
+  </div>
---
+++ Training/resources/views/layout/admin.blade.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Home</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<style>
+body {
+  margin: 0;
+  font-family: "Lato", sans-serif;
+}
+
+.sidebar {
+  margin: 0;
+  padding: 0;
+  width: 200px;
+  background-color: #f1f1f1;
+  position: fixed;
+  height: 100%;
+  overflow: auto;
+}
+
+.sidebar a {
+  display: block;
+  color: black;
+  padding: 16px;
+  text-decoration: none;
+}
+ 
+.sidebar a.active {
+  background-color: green;
+  color: white;
+}
+
+.sidebar a:hover:not(.active) {
+  background-color: #555;
+  color: white;
+}
+
+div.content {
+  margin-left: 200px;
+  padding: 1px 16px;
+  height: 1000px;
+}
+
+@media screen and (max-width: 700px) {
+  .sidebar {
+    width: 100%;
+    height: auto;
+    position: relative;
+  }
+  .sidebar a {float: left;}
+  div.content {margin-left: 0;}
+}
+
+@media screen and (max-width: 400px) {
+  .sidebar a {
+    text-align: center;
+    float: none;
+  }
+}
+</style>
+<body></body>
+<div class="row">
+ @include('includes.sidebar')
+  <div class="col-10"> 
+      <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <div class="container">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/aboutus">About us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/service">Service</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      @yield('content')
+  </div>
+</div>
+</body>
+</html>
